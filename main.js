bce581be2a82ad712cf07269f44fa83cf08899fd
Eric Chen <eric.chen@wsu.edu>
2020-07-24 15:34:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-$(document).ready(function() {
-    $(this).keydown(function(e) {
-    		e.preventDefault();
-        alert();
-    });
+var count = 0;
+
+$("button#clear").click(function(){
+  count = 0;
+  $("#content").text(count);
 });
+
+$(this).keydown(function(e) {
+   e.preventDefault();
+   count+=1;
+   $("#content").text(count);
+});
+
+$(document).ready(function() {
+    $("#content").text(count)
+});
+
