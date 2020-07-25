82bd7acbf06cc16dfb84d92cf0114c0f16029c60
Eric Chen <eric.chen@wsu.edu>
2020-07-24 17:54:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 var count = 0;
 
-
-
-$(this).keydown(function(e) {
-   e.preventDefault();
-   count+=1;
-   $("#content").text(count);
-   $("#timestamp").text("Last Clicked: " + new Date().toLocaleString());
-});
-
 $(document).ready(function() {
     $("#content").text(count)
   
     $(":button").click(function(){
-    console.log("cleared")
-    count = 0;
-    $("#content").text(count);
-    $("#timestamp").text(null);
-  });
+       console.log("cleared")
+       count = 0;
+       $("#content").text(count);
+       $("#timestamp").text(null);
+    });
+   
+    $(this).keydown(function(e) {
+       e.preventDefault();
+       count+=1;
+       $("#content").text(count);
+       $("#timestamp").text("Last Clicked: " + new Date().toLocaleString());
+    });
 });
